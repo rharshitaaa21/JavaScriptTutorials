0c72f109aecd76d84d8b703174cc5402ae9964fc
Harshita Raghav <raghav.harshita21@gmail.com>
2022-09-03 01:41:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//printing methods ---------------
+console.log("This is print method-1");
+document.write("This is print method-2");
+
+console.log("Hello World", 4 + 6, "Another log");
+//alert("Me");
+console.warn("This is warning");
+console.error("This is an error");
---
+++ variables.js
@@ -0,0 +1,25 @@
+// varaiables
+var num2 = "Dee";
+console.log(num1+num2);
+
+// data types -----------
+var str1 = "This is a string";
+var str2 = "Harshita Raghav";
+var num1 = 1000;
+
+var marks = {
+    dee: 100,
+    mad: 90,
+    mukul: 80
+};
+console.log(marks);
+var a = true ;
+var b = false;
+console.log(a, b);
+
+/*
+ two type of data types
+ 1. primitve - undefined, null, numbers, strings, boolean, symbols
+ 2. refrence- arrays, objects
+ */
+
