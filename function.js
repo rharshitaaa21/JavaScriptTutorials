7abd3f4e86d990ae4b0125ad1c25422e4359d87b
Harshita Raghav <raghav.harshita21@gmail.com>
2022-09-03 13:05:06 +0530

    Added Functions

---
+++ function.js
@@ -0,0 +1,21 @@
+//DRY = do not repeat yourself
+
+//1--------------------
+function avg(a,b){
+    return a+ b;
+}
+var c = avg(2,3);
+document.write(c)
+//2------------------------
+
+function print( a, b)
+{
+
+    console.log( "Harshita Raghav ");
+    console.log(" Madhur Sharma");
+    return a+b;
+    
+}
+var ans = print( 10,20);
+
+//on console, number are described blue and strings are described red
