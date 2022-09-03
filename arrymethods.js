01bd7b13a8d7a9e04b2eaa6c47b8867af1a2eb87
Harshita Raghav <raghav.harshita21@gmail.com>
2022-09-03 23:31:41 +0530

    added files

---
+++ arrymethods.js
@@ -0,0 +1,16 @@
+let arr =["dee", "mad", "mannu", "puhu" ];
+// array methods 
+console.log( arr.length);
+//arr.pop(); // pops out the last element
+arr.push("oyo chlo  ");
+//arr.shift(); // pops out the first element  
+ console.log(arr);
+
+ const len = arr.unshift( "Chl lo na ");
+ console.log( len);
+ console.log(arr);
+
+
+ let arr1 = [10,47,27,84,20,28];
+ arr1.sort(); 
+ console.log( arr1);
---
+++ breakContinue.js
@@ -0,0 +1,3 @@
+ // break --> gets out of loop immediately 
+ // continue --> gets into next interation immediately 
+ 
