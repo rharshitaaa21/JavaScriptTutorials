f3d340885f490eacaf1a7971669fb1074046a794
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-09-29 22:38:43 +0530

    Updated DOM File

--- DOM.js
+++ DOM.js
@@ -13,9 +13,32 @@
 // console.log(heading);
 
 // 4. querySelector 
-// const  heading = document.querySelector('firstContainer');
-// console.log(heading);
+// const  heading = document.querySelector('flag');
+//  console.log(flag);
 
 //5. querySelectorAll 
-const heading=  document.querySelectorAll('heading');
-console.log(heading);
+//const heading=  document.querySelectorAll('.flag');
+//console.log(heading);
+
+//DOM Traversal ---------------
+//1. parent node 
+// const heading = document.querySelector('.heading');
+// const parent = heading.parentNode;
+// console.log(parent);
+
+//2. children nodes
+// const parent = document.querySelector('.parent');
+// console.log(parent.childNodes);
+
+//3. nextElementSibling
+// const heading = document.querySelector('.heading');
+// console.log(heading.nextElementSibling);
+
+
+//4. previousElementSibling 
+// const subHeading = document.querySelector('h3');
+// console.log(subHeading.previousElementSibling);
+
+//5. manipulation
+// const heading = document.querySelector('.flag');
+//  heading.innerHTML= "I am tired of this stress";
